1ca202ac1d30c8d45f18174423c4d85dd3cf7a90
dell <you@example.com>
2024-04-11 20:45:10 +0530

    Add project files.

---
+++ DoWhileLoop/Program.cs
@@ -0,0 +1,13 @@
+﻿Random radm = new Random();
+
+int curent = 0;
+int indx = 0;
+
+do
+{
+    curent = radm.Next(1,15);
+    indx++;
+    Console.WriteLine($"{indx} - {curent}");
+
+}
+while (curent < 7);
